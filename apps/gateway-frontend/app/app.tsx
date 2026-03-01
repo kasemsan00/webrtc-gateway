72d58707f13d0d10d39a2654512aa623221a4c4c
Kasemsan Chompuwised <106854077+kasemsan00@users.noreply.github.com>
2026-03-01 15:49:44 +0700

    Initial commit

---
+++ apps/gateway-frontend/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@webrtc-gateway/gateway-frontend" />
+    </div>
+  );
+}
+
+export default App;
